eb9303c5ee6f0116c4332ac84ab3de82cd3dc099
z3nth10n <z3nth10n@gmail.com>
2020-03-28 03:18:02 +0100

    Documented RuinsDebug.debug var

--- src/main/java/atomicstryker/ruins/common/RuinsDebug.java
+++ src/main/java/atomicstryker/ruins/common/RuinsDebug.java
@@ -1,5 +1,6 @@
 package atomicstryker.ruins.common;
 
 public class RuinsDebug {
-    public static final boolean debug = true;
+	// A constant value that enable debug (System.out.println) and creates one dungeon near the player when the criteria is met (y >= 400).
+    public static final boolean debug = false;
 }
